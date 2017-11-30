4a2a1e29a294912e2c0d6b7fc82eed9b365ebdc1
andersongomes001 <comprasgomes@hotmail.com>
2017-11-29 22:26:12 -0200

    novos componentes adicionados ao projeto

---
+++ src/app/components/portifolio/portifolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portifolio',
+  templateUrl: './portifolio.component.html'
+})
+export class PortifolioComponent {
+
+  constructor() { }
+
+
+}
